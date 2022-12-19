051a89b989d32eebfb87295d866e35c1e813b5bf
kartikth40 <kartikth40@gmail.com>
2022-12-19 14:00:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'
+// import './App.css'
+import styled from 'styled-components'
+import Character from './components/gameComponents/Character'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <GameContainer>
+      <Character />
+    </GameContainer>
+  )
 }
 
-export default App;
+export default App
+
+const GameContainer = styled.div`
+  background-color: hsl(350, 100%, 80%);
+  width: 100vw;
+  height: 100vh;
+`
